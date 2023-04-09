939077e4906dbe2a5e1aad5c4151555f050d59d3
jairoromero <jromero.alumno@iesrafaelalberti.es>
2023-04-09 20:34:12 +0200

    characters style

--- recureact/src/components/Character.jsx
+++ recureact/src/components/Character.jsx
@@ -5,14 +5,15 @@ const Character = ({id,nombre,fotopersonaje,role,fotorol,fondo}) => {
 
 
   return (
-    <li key={id} className='character' style={{ backgroundImage: `url(${fondo})` }} >
-        <img src={fotopersonaje} />
+    <li key={id} className='character' >
+        <div className='img' style={{ backgroundImage: `url(${fondo})` }}><img src={fotopersonaje} /></div>
         <div className='infopersonaje'>
             <div className='texto'>
-               <h1>nombre:{nombre}</h1>
-                <h1>rol:{role}</h1> 
+               <h1>Name: {nombre}</h1>
+                <h1>Role: {role}</h1>
+                <img src={fotorol}/> 
             </div>
-            <img src={fotorol}/>
+            
         </div>
     </li>
   )
