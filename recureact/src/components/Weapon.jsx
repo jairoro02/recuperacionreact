36bda16e440fcb133bd3eff6629f62e384bf6731
jairoro <jromero.alumno@iesrafaelalberti.es>
2023-04-11 13:20:53 +0200

    weapons render

--- recureact/src/components/Weapon.jsx
+++ recureact/src/components/Weapon.jsx
@@ -1,17 +1,38 @@
 import React from 'react'
 
-const Weapon = ({uuid, name,img,category,firerate,magazine, cost}) => {
-    console.log('gayyyyyy')
+const Weapon = ({list}) => {
+
   return (
-    <div key={uuid} className='weapon'>
-        <img src={img} />
-        <div className='info'>
-            <h1>Category: {category}</h1>
-            <h1>Name: {name}</h1>
-            <h1>Price: {cost}</h1>
-            <h1>Firerate: {firerate}</h1>
-            <h1>Bullets: {magazine}</h1>
-        </div>
+    <div className='weapons'>
+      {list.map((weapon)=>{
+         if(weapon.shopData === null){
+          console.log(weapon)
+          return (
+            <div key={weapon.uuid} className='weapon'>
+                <img src={weapon.displayIcon} />
+                <div className='info'>
+                    <h1>Category: Mele</h1>
+                    <h1>Name: {weapon.displayName}</h1>
+                </div>
+            </div>
+          )
+         }
+         else{
+          return (
+            <div key={weapon.uuid} className='weapon'>
+                <img src={weapon.displayIcon} />
+                <div className='info'>
+                    <h1>Category: {weapon.shopData.category}</h1>
+                    <h1>Name: {weapon.displayName}</h1>
+                    <h1>Price: {weapon.shopData.cost}</h1>
+                    <h1>Firerate: {weapon.weaponStats.fireRate}</h1>
+                    <h1>Bullets: {weapon.weaponStats.magazineSize}</h1>
+                </div>
+            </div>
+          )
+         }
+         
+      })}
     </div>
   )
 }
--- recureact/src/pages/Weapons.jsx
+++ recureact/src/pages/Weapons.jsx
@@ -7,26 +7,16 @@ const Weapons = () => {
   const [weapons, setWeapons] = useState([]);
 
     const url = 'https://valorant-api.com/v1/weapons'
-
-
-    useApi(url).then(response=>{
-        setWeapons(response)
-    })
+    
     useEffect(()=>{
-        useApi(url)
+      useApi(url).then(response=>{
+        setWeapons(response)
+      })
     },[])
-
-    weapons.map((weapon)=>{
-      console.log('pasaaaaaaaaaaaaaaaa')
-      if(weapon.shopData === null){
-        return <Weapon uuid={weapon.uuid} name={weapon.displayName} img={weapon.displayIcon} category='Mele' firerate='0' magazine='Without' cost='0' />
-      }
-      else{
-        return <h1>España</h1>
-      }
-     
-    })
-  
+    if(weapons.length!=0){
+      return <Weapon list={weapons} />
+    }
+      
 }
 
 export default Weapons
